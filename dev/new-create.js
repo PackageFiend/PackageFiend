987dbabb64f1f50594481fbba87182025e618dff
Ben Keener <keener4christ@gmail.com>
2020-06-01 14:30:46 -0400

    Made new script

---
+++ dev/new-create.js
@@ -0,0 +1,26 @@
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+  region: 'us-east-1',
+  endpoint: 'http://localhost:8000'
+});
+
+function main() {
+  const params = {
+    TableName: 'Users',
+    KeySchema: [
+      { AttributeName: 'username', KeyType: 'HASH' }
+    ],
+    AttributeDefinitions: [
+      { AttributeName: 'username', AttributeType: 'S' }
+    ],
+    BillingMode: 'PAY_PER_REQUEST'
+  };
+
+  try {
+    const data = await dynamodb.createTable(params);
+    console.log('Created table:', JSON.stringify(data, null, 2));
+  } catch (err) {
+    console.error('Error creating table:', JSON.stringify(err, null, 2));
+  }
+}
