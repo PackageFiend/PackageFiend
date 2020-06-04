375a49bebcae07695edae7ad140533e623b5c5a5
Ben Keener <keener4christ@gmail.com>
2020-06-04 19:25:22 -0400

    Messed around with relative paths

--- routes/track.js
+++ routes/track.js
@@ -8,4 +8,15 @@ router.get('/num/:tracknum', async (req, res) => {
   res.send(JSON.stringify(await track.track(trackingNumber), null, 2));
 });
 
+router.get('/:tracknum', (req, res) => {
+  res.header('X-Track-Id', req.params.tracknum);
+  res.sendFile('tracker.html', {root: 'web_files'});
+});
+
+router.get('/static/*', function(req, res){
+    var uid = req.params.uid,
+        path = req.params[0] ? req.params[0] : 'index.html';
+    res.sendFile(path, {root: 'web_files'});
+});
+
 module.exports = router;
