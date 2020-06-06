d4f072df0873af9ed1bb2d71818ea927b46cc3bd
Nehal Shastri <nhlshstr@gmail.com>
2020-06-05 19:46:51 -0700

    changed silly order mistake

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const port = 8080;
 // Set static folder
 app.use(express.static(path.join(__dirname, 'web_files')));
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
-
 app.get('*', function (req, res, next) {
   if (req.get('X-Forwarded-Proto') === 'https') { next(); } else {
     res.set('X-Forwarded-Proto', 'https');
     res.redirect('https://' + req.get('host') + req.url);
   }
 });
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
