c17c3cde511cd07d77383f3a894fb7a071d3ab53
Nehal Shastri <nhlshstr@gmail.com>
2020-06-05 19:26:21 -0700

    redirecting to https

--- server.js
+++ server.js
@@ -6,4 +6,11 @@ const port = 8080;
 // Set static folder
 app.use(express.static(path.join(__dirname, 'web_files')));
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+
+app.get('*', function (req, res, next) {
+  if (req.get('x-forwarded-proto') === 'https') { next(); } else {
+    res.set('x-forwarded-proto', 'https');
+    res.redirect('https://' + req.get('host') + req.url);
+  }
+});
