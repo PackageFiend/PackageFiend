f952b77e8bc9d55f9d7aadcb66cd8f4aa5250e7e
Ben Keener <keener4christ@gmail.com>
2020-06-06 15:29:07 -0400

    Fixed to single array

--- routes/track.js
+++ routes/track.js
@@ -5,7 +5,7 @@ const track = require('../track');
 router.get('/num/:tracknum', async (req, res) => {
   const trackingNumber = req.params.tracknum;
 
-  res.send(JSON.stringify(await track.track(trackingNumber), null, 2));
+  res.send(JSON.stringify(await track.track([trackingNumber]), null, 2));
 });
 
 module.exports = router;
