82c22d26242f29a2c4c067843269b147c470ce27
Beardocracy <travisjbearden@gmail.com>
2020-06-09 11:49:54 -0700

    Added collapsible toggle for new account creation

--- web_files/scripts/signin.js
+++ web_files/scripts/signin.js
@@ -1,17 +1,9 @@
-// Dropdown for multiple tracking numbers
-const collapsible = document.querySelector('.current_displayed_collapsible');
-collapsible.addEventListener("click", event => {
-    collapsible.classList.toggle('active');
-    const nums = document.querySelector('.additional_track_nums');
-    nums.classList.toggle('add_auto_height');
+// Create account collapsible
+const collapse_button = document.querySelector('.no_account');
+collapse_button.addEventListener("click", event => {
+    collapse_button.classList.toggle('active');
+    const collapsible = document.querySelector('.create_collapsible');
+    collapsible.classList.toggle('create_uncollapsed');
+    const signin = document.querySelector('.sign_in_box');
+    signin.classList.toggle('inactive');
 });
-
-// Map settings
-let mymap = L.map('map').setView([39.8283, -98.5795], 3);
-        
-L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=fEH52Z1zwyyCZ7gf1vZA', {
-    attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
-}).addTo(mymap);
-
-const Amishtown = L.marker([40.0379, -76.3055]).addTo(mymap);
-const circle = L.circle([])
