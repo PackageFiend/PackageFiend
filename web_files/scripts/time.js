ad8d0a5b5d1aa446bae5e4c01f034cd901845766
Ben Keener <keener4christ@gmail.com>
2020-06-09 15:17:56 -0400

    Added some dynamics

--- web_files/scripts/time.js
+++ web_files/scripts/time.js
@@ -6,4 +6,26 @@ $(document).ready(function() {
     console.log(fTime);
     $(this).text(fTime);
   });
+
+  $('.track_data_line').click(function () {
+    if ($(this).hasClass('tdl_bold')) return;
+    $('.tdl_bold').removeClass('tdl_bold');
+    $(this).addClass('tdl_bold');
+
+    const pid = $(this).children('.data_line_l').children('.data_line_num').eq(0).text();
+
+    let parcel = null;
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].TrackNum === pid) {
+        parcel = data[i];
+        break;
+      }
+    }
+
+    $('.tracking_number').text(parcel.TrackNum);
+    $('.track_number_title .carrier_icon_bold').text(parcel.Provider);
+
+    console.log(parcel);
+    
+  });
 });
