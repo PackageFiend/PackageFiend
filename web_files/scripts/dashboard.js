e98dd237e14e419d86be4086c1287042947e38b5
Beardocracy <travisjbearden@gmail.com>
2020-06-11 11:54:54 -0700

    Added bar graph

--- web_files/scripts/dashboard.js
+++ web_files/scripts/dashboard.js
@@ -1,3 +1,4 @@
+// Accordion dropdowns
 $(document).ready(function(){
     $(".alerts_collapsible").click(function(){
         $(".alerted_nums").toggleClass("add_five_lines");
@@ -10,26 +11,7 @@ $(document).ready(function(){
     });
 });
 
-// Dropdown for multiple tracking numbers
-const collapsible = document.querySelector('.current_displayed_collapsible');
-collapsible.addEventListener("click", event => {
-    collapsible.classList.toggle('active');
-    const nums = document.querySelector('.additional_track_nums');
-    nums.classList.toggle('add_auto_height');
-});
-
-// Map settings
-let mymap = L.map('map').setView([39.8283, -98.5795], 3);
-        
-L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=fEH52Z1zwyyCZ7gf1vZA', {
-    attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
-}).addTo(mymap);
-
-//const Amishtown = L.marker([40.0379, -76.3055]).addTo(mymap);
-//const circle = L.circle([])
-
-
-
+// Placeholder data for developing
 const user_data_fake = {
     "username": "shootermcgavinsucks",
     "name": "Happy Gilmore",
