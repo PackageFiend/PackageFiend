8b738b3466847b7b58eb00d173932201590511c0
Ben Keener <keener4christ@gmail.com>
2020-06-11 22:36:35 -0400

    Added script for predash

---
+++ web_files/scripts/predash.js
@@ -0,0 +1,22 @@
+async function main () {
+  console.log('Ready');
+
+  if (!localStorage.pkgfnd_token) {
+    console.error('No token');
+    window.location = 'localhost:8080';
+    return;
+  }
+
+  const dashdat = await axios.get('http://localhost:8080/user/dashboard',
+    {
+      headers: {
+        Authorization: `Bearer ${localStorage.pkgfnd_token}`
+      }
+    });
+
+  //console.log(dashdat);
+
+  $('body').html(dashdat.data);
+}
+
+main();
