b9da47514dfc9187abec23ce76a3f9d2ac1f2336
Beardocracy <travisjbearden@gmail.com>
2020-06-16 15:21:29 -0700

    Added enter key event handler

--- web_files/scripts/homepage.js
+++ web_files/scripts/homepage.js
@@ -2,13 +2,8 @@ const uspsReg = /^(?:9(?:4|2|3)|EC|CP|82)\d+(?:EA)?\d+(?:US)?$/;
 const upsReg = /^1Z[A-Z0-9]+$/;
 const fedexReg = /^(?:\d{12}|\d{15}|\d{20})$/;
 
-$(document).ready(() => {
-  if (localStorage.pkgfnd_name) {
-    $('.sign_in_box').html('<a href="http://localhost:8080/track/dashboard">Dashboard</a> Hello, ' + localStorage.pkgfnd_name);
-  }
-
-  $('.go_button').click(() => {
-    const tnums = $('.enter_numbers_box input').val();
+function send_to_tracking() {
+  const tnums = $('.enter_numbers_box input').val();
     const numsList = tnums.split(',');
     let err = false;
 
@@ -25,5 +20,25 @@ $(document).ready(() => {
     if (tnums !== "" && !err) {
       window.location = `http://localhost:8080/track/num/${tnums.replace(/\s/g, '')}`;
     }
+}
+
+
+$(document).ready(() => {
+  if (localStorage.pkgfnd_name) {
+    $('.sign_in_box').html('<a href="http://localhost:8080/track/dashboard">Dashboard</a> Hello, ' + localStorage.pkgfnd_name);
+  }
+
+  $('.go_button').click(() => {
+    send_to_tracking();
   });
+
+  $('.enter_numbers_box input').keypress(function(event){
+    const keycode = event.which;
+    if(keycode == 13){
+      // Keeps form from clearing
+      event.preventDefault();
+      send_to_tracking();
+    }
+  });
+  
 });
