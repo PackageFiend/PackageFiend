13a98e82378dbad8af8810b71376780d4676734a
Beardocracy <travisjbearden@gmail.com>
2020-06-16 16:51:49 -0700

    Fixed indentation

--- web_files/scripts/homepage.js
+++ web_files/scripts/homepage.js
@@ -4,22 +4,22 @@ const fedexReg = /^(?:\d{12}|\d{15}|\d{20})$/;
 
 function send_to_tracking() {
   const tnums = $('.enter_numbers_box input').val();
-    const numsList = tnums.split(',');
-    let err = false;
+  const numsList = tnums.split(',');
+  let err = false;
 
-    $('.regex_errors').empty();
+  $('.regex_errors').empty();
 
-    for (let i = 0; i < numsList.length; i++) {
-      const num = numsList[i].trim();
-      if (!uspsReg.test(num) && !upsReg.test(num) && !fedexReg.test(num)) {
-        $('.regex_errors').append(`<div class="err_num">${num} is not a valid tracking number.</div>`);
-        err = true;
-      }
+  for (let i = 0; i < numsList.length; i++) {
+    const num = numsList[i].trim();
+    if (!uspsReg.test(num) && !upsReg.test(num) && !fedexReg.test(num)) {
+      $('.regex_errors').append(`<div class="err_num">${num} is not a valid tracking number.</div>`);
+      err = true;
     }
-    
-    if (tnums !== "" && !err) {
-      window.location = `http://localhost:8080/track/num/${tnums.replace(/\s/g, '')}`;
-    }
+  }
+  
+  if (tnums !== "" && !err) {
+    window.location = `http://localhost:8080/track/num/${tnums.replace(/\s/g, '')}`;
+  }
 }
 
 
